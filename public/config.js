8405f24da516ac99ae8fc09864f57744c6ad6e92
Michael Elekwachi <41022512-mcelekwachi@users.noreply.replit.com>
2025-04-05 12:41:33 +0000

    📦 Move Firebase config to config.js and clean login.html

---
+++ public/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzNR3ooG0WDQGwcPsvKcwH81df4J-K7HM",
+  authDomain: "kickback-52b47.firebaseapp.com",
+  projectId: "kickback-52b47",
+  storageBucket: "kickback-52b47.appspot.com",
+  messagingSenderId: "70792222176",
+  appId: "1:70792222176:web:18d9d819a53f9790214785"
+};
